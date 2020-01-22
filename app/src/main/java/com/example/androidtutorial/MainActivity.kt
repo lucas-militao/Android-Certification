dc5f91c92d7778e4858ec8273e3cef4e858597a7
Lucas Militão <lucasmengineer@edu.unifor.br>
2020-01-22 14:42:19 -0300

    notificacao implementada

--- app/src/main/java/com/example/androidtutorial/MainActivity.kt
+++ app/src/main/java/com/example/androidtutorial/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.example.androidtutorial
 
+import android.annotation.SuppressLint
+import android.annotation.TargetApi
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
-import com.google.android.material.snackbar.Snackbar
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val CHANNEL_ID = "1"
+    private var builder : Notification.Builder? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,12 +29,32 @@ class MainActivity : AppCompatActivity() {
         setupView()
     }
 
+    @SuppressLint("WrongConstant")
     private fun setupView() {
-        showNotificationButton.setOnClickListener(showSnackbar())
+
+        showNotificationButton.setOnClickListener(showNotification())
     }
 
-    private fun showSnackbar() = View.OnClickListener {
-        var snackbar = Snackbar.make(principal, "Notificando aqui oh!", Snackbar.LENGTH_SHORT)
-        snackbar.show()
+    @SuppressLint("WrongConstant")
+    @TargetApi(Build.VERSION_CODES.O)
+    private fun showNotification() = View.OnClickListener {
+
+        var notificationId = 123
+
+        var intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+
+        var pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+        var notification = Notification.Builder(this)
+            .setSmallIcon(R.drawable.ic_android)
+            .setContentTitle("Minha notificacao")
+            .setContentText("Uma nova notificacao")
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        var notifyMgr : NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notifyMgr.notify(notificationId, notification.build())
     }
 }
