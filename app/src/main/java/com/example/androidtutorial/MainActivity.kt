c7f460c88a715193b8374776efa0c03518eb3e2f
Lucas Militão <lucasmengineer@edu.unifor.br>
2020-01-22 13:20:08 -0300

    snackbar implementado

--- app/src/main/java/com/example/androidtutorial/MainActivity.kt
+++ app/src/main/java/com/example/androidtutorial/MainActivity.kt
@@ -2,14 +2,12 @@ package com.example.androidtutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
 import android.view.View
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private var toast : Toast? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,22 +17,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupView() {
-        toastTopButton.setOnClickListener(notificationOnTop())
-        toastBottomButton.setOnClickListener(notificationOnBottom())
+        showNotificationButton.setOnClickListener(showSnackbar())
     }
 
-    private fun notificationOnTop() = View.OnClickListener() {
-        toast?.cancel()
-        toast = null
-        toast = Toast.makeText(this, getString(R.string.notification_top), Toast.LENGTH_LONG)
-        (toast as Toast).setGravity(Gravity.TOP, 0 , 0)
-        (toast as Toast).show()
-    }
-
-    private fun notificationOnBottom() = View.OnClickListener() {
-        toast?.cancel()
-        toast = null
-        toast = Toast.makeText(this, getString(R.string.notification_bottom), Toast.LENGTH_LONG)
-        (toast as Toast).show()
+    private fun showSnackbar() = View.OnClickListener {
+        var snackbar = Snackbar.make(principal, "Notificando aqui oh!", Snackbar.LENGTH_SHORT)
+        snackbar.show()
     }
 }
