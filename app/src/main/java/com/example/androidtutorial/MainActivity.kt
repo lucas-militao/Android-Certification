96fae4f9cde0874702eb16b5a9450fdc3c95c6fc
Lucas Militão <lucasmengineer@edu.unifor.br>
2020-01-24 10:45:16 -0300

    Implementado método que ler Qrcode utilizando a biblioteca ZXing

--- app/src/main/java/com/example/androidtutorial/MainActivity.kt
+++ app/src/main/java/com/example/androidtutorial/MainActivity.kt
@@ -1,57 +1,75 @@
 package com.example.androidtutorial
 
 import android.annotation.SuppressLint
-import android.annotation.TargetApi
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.content.Context
 import android.content.Intent
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
-import androidx.annotation.RequiresApi
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.zxing.Result
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.random.Random
+import me.dm7.barcodescanner.zxing.ZXingScannerView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ZXingScannerView.ResultHandler {
 
-    lateinit var diceImage: ImageView
+    private lateinit var scannerView: ZXingScannerView
+    val MY_PERMISSIONS_REQUEST_ACCESS_CAMERA = 0
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val rollButton: Button = findViewById(R.id.rollButton)
-        rollButton.text = "Lets roll"
-        rollButton.setOnClickListener{
-            rollDice()
-        }
 
-        diceImage = findViewById(R.id.diceImage)
+        scannerView = findViewById(R.id.zxingScanner)
+
+        setupView()
     }
 
-    private fun rollDice() {
+    public fun setupView() {
+        requestCameraPermission()
+    }
 
-        val random = Random.nextInt(6) + 1
+    private fun requestCameraPermission() {
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "You must give the permission", Toast.LENGTH_SHORT).show()
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    android.Manifest.permission.CAMERA)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(android.Manifest.permission.CAMERA),
+                    MY_PERMISSIONS_REQUEST_ACCESS_CAMERA)
 
-        val drawableResource = when (random) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            6 -> R.drawable.dice_6
-            else -> R.drawable.empty_dice
+                scannerView.setResultHandler(this)
+                scannerView.startCamera()
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+            scannerView.setResultHandler(this)
+            scannerView.startCamera()
         }
-        diceImage.setImageResource(drawableResource)
+    }
+
+    override fun onDestroy() {
+        scannerView.stopCamera()
+        super.onDestroy()
+    }
+
+    override fun handleResult(p0: Result?) {
+        resultText.text = p0?.text
     }
 }
