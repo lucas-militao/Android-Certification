af2715a90581c44ada090dadd159b0a1dc69c3cb
lumilitas <lucas23121995>
2020-01-27 19:13:24 -0300

    implementando About Me app

--- app/src/main/java/com/example/androidtutorial/MainActivity.kt
+++ app/src/main/java/com/example/androidtutorial/MainActivity.kt
@@ -9,32 +9,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var toast : Toast? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        setupView()
-    }
-
-    private fun setupView() {
-        toastTopButton.setOnClickListener(notificationOnTop())
-        toastBottomButton.setOnClickListener(notificationOnBottom())
-    }
-
-    private fun notificationOnTop() = View.OnClickListener() {
-        toast?.cancel()
-        toast = null
-        toast = Toast.makeText(this, getString(R.string.notification_top), Toast.LENGTH_LONG)
-        (toast as Toast).setGravity(Gravity.TOP, 0 , 0)
-        (toast as Toast).show()
-    }
-
-    private fun notificationOnBottom() = View.OnClickListener() {
-        toast?.cancel()
-        toast = null
-        toast = Toast.makeText(this, getString(R.string.notification_bottom), Toast.LENGTH_LONG)
-        (toast as Toast).show()
     }
 }
